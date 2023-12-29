b188dc0d9523cdcd0140f6ccef39fc07fcc7a32d
danil emelin <danil228aeedj@mail.ru>
2023-12-29 14:08:28 +0300

    create linked list

---
+++ structures/linked_list/linked_list.c
@@ -0,0 +1,76 @@
+#include "linked_list.h"
+struct list *node_create(int64_t value) {
+  struct list *node = malloc(sizeof(struct list));
+  if (!node)
+    return NULL;
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
+void list_add_front(struct list **old, int64_t value) {
+  if (!old)
+    return;
+  struct list *new_node = node_create(value);
+  if (!new_node)
+    return;
+  new_node->next = *old;
+  *old = new_node;
+}
+size_t list_length(const struct list *list) {
+  size_t length = 0;
+  while (!list) {
+    length++;
+    list = list->next;
+  }
+  return length;
+}
+void list_destroy(struct list **list_ref) {
+  if (!list_ref)
+    return;
+  struct list *list = *list_ref;
+  struct list *copy = NULL;
+  while (list->next) {
+    copy = list;
+    list = list->next;
+    free(copy);
+  }
+  *list_ref=NULL;
+}
+
+struct list *list_last(struct list *list) {
+  if (!list)
+    return NULL;
+  while (list->next) {
+    list = list->next;
+  }
+  return list;
+}
+void list_add_back(struct list **old, int64_t value) {
+  if (!old)
+    return;
+  struct list *first = *old;
+  while (first->next) {
+    first = first->next;
+  }
+  struct list *new_node = node_create(value);
+  first->next = new_node;
+}
+void list_print(struct list const *first) {
+  puts("Printing list");
+  puts("-------------------------");
+  while (first) {
+    printf("%" PRId64 "\n", first->value);
+    first = first->next;
+  }
+  puts("-------------------------");
+}
+
+int main(void) {
+
+  struct list *first = node_create(25);
+  list_add_front(&first, 16);
+  list_add_back(&first, 32);
+  list_print(first);
+  list_destroy(&first);
+  list_print(first);
+}
---
+++ structures/linked_list/linked_list.h
@@ -0,0 +1,25 @@
+//
+// Created by danil on 12/28/23.
+//
+
+#ifndef SANDBOX_LINKED_LIST_H
+#define SANDBOX_LINKED_LIST_H
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+struct list {
+  int64_t value;
+  struct list *next;
+};
+struct list *node_create(int64_t value);
+void list_add_front(struct list **old, int64_t value);
+size_t list_length(const struct list *list);
+void list_destroy(struct list **list);
+struct list *list_last(struct list *list);
+void list_print(struct list const *first);
+void list_add_back(struct list **old, int64_t value);
+int64_t list_sum(const struct list *list);
+struct maybe_int64 list_at(const struct list *list, size_t idx);
+struct list *list_reverse(const struct list *list);
+#endif // SANDBOX_LINKED_LIST_H
